7ac246bf10b35f8b9fa79cf76ee30b96e8389c7d
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-10 12:06:29 +0100

    corretto errore in else (rimosso parentesi tonde)

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,13 +1,18 @@
 const ageA = parseInt(prompt("Inserisci gli anni della persona A"))
 const ageB = parseInt(prompt("Inserisci gli anni della persona B"))
 
-console.log(ageA)
-console.log(ageB)
 
 if( ageA > ageB){
-    console.log(ageA)
-} else if (ageB > ageA){
-    console.log(ageB)
+
+    console.log(ageA);
+} 
+else if (ageB > ageA){
+
+    console.log(ageB);
+}
+else{
+    
+    console.log("le età sono uguali");
 }
 
 
