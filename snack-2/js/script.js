c3a2ababf718ab1bc36b7a57619bb9572ac4c216
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-14 12:17:52 +0100

    inserito controllo in console

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,6 +1,7 @@
 let listItemElement = document.createElement('li')
 
 for (let i=1; i<=6; i++){
+    console.log(`inserito nuovo li ${listItemElement}`)
     listItemElement.innerHTML = i
     document.querySelector('ul').appendChild(listItemElement)
 }
