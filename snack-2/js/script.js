1770b599a1e3f417f04335bd72ba017c9154f149
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-14 12:14:54 +0100

    inserite operazioni ciclo for

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,5 +1,6 @@
 let listItemElement = document.createElement('li')
 
 for (let i=1; i<=6; i++){
-    
+    listItemElement.innerHTML = i
+    document.querySelector('ul').appendChild(listItemElement)
 }
