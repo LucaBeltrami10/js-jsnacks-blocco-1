1a986f2ee41f120c1dec33fb14c177d61645e55d
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-14 12:44:11 +0100

    snack 3 completato

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,5 +1,5 @@
 
-for (let i=1; i<=6; i+1){
+for (let i=0; i<=10; i++){
     console.log(i)
-    document.querySelector('ul').innerHTML += `<li> ${i}</li>`
+    document.querySelector('ul').innerHTML += `<li> ${i * 2}</li>`
 }
