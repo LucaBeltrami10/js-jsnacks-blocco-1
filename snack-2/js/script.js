70c5ef8998b3206d2945f199361a19b51ee216df
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-14 12:40:51 +0100

    nuovo snack

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,6 +1,14 @@
-
+let listItemElement = document.createElement('li')
+let ulElement = document.querySelector('ul')
 
 for (let i=1; i<=6; i++){
+    console.log(`inserito nuovo`)
+    listItemElement.innerHTML = i
+    ulElement.appendChild(listItemElement)
+}
+
+
+/* for (let i=1; i<=6; i++){
     console.log(i)
     document.querySelector('ul').innerHTML += `<li> ${i}</li>`
-}
+} */
---
+++ snack-3/js/script.js
@@ -0,0 +1,5 @@
+
+for (let i=1; i<=6; i+1){
+    console.log(i)
+    document.querySelector('ul').innerHTML += `<li> ${i}</li>`
+}
