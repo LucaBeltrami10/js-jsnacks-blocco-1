1eedd0d74ab364e2eb97aaffc3a22917f9dfe983
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-15 12:22:07 +0100

    commentato print in console

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -4,7 +4,7 @@ const numeriUtili = []
 console.log(`numeri utili a inizio esercizio (teoricamente nessuno): ${numeriUtili}`)
 
 for (let i=0; i<100; i++){
-    console.log(i)
+    /* console.log(i) */
 
     if(i%7 == 0 || i%8 == 0){
         numeriUtili.push(i)
