da262d93f687b5397f6554f4da182ea888a8b32e
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 15:59:04 +0100

    inserito prompt in ciclo

--- Snack A/script.js
+++ Snack A/script.js
@@ -6,5 +6,6 @@ const numberOfRequest = 6
 
 /* creo ciclo in cui viene richiesto X volte all'utente di inserire un numero */
 for (let i=1; i<=6; i++){
+    let requestNumber = prompt('inserisci un numero da 1 a 100')
 }
     /* comparo il numero inserito. se è dispari lo inserisco in lista sennò vado avanti*/
