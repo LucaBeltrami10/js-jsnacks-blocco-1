9a86365b5145a41733d724614ec5cf951e43c147
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 18:15:50 +0100

    inserito ciclo con condizione

--- Snack C/script.js
+++ Snack C/script.js
@@ -1,20 +1,36 @@
 /* chiedo all'utente i due numeri */
-let smallerNum = prompt('inserisci un numero composto da 2 numeri')
-const largerNum = prompt('inserisci un numero composto da 4 numeri')
+let smallerNum = parseInt(prompt('inserisci un numero composto da 2 numeri'))
+const largerNum = parseInt(prompt('inserisci un numero composto da 4 numeri'))
 
 /* mostro in console i inumeri inseriti */
 
 console.log(smallerNum)
 console.log(largerNum)
 
+inputPrint = smallerNum > largerNum
 
-/* condizione: è il numero minore (dal primo cilo: moltiplicato x 2) ancora minore? ripeto il ciclo */
+const listaNumeriMaggiori = []
+
 /* if(smallerNum <= largerNum){
     smallerNum *= 2
 }else if(smallerNum > largerNum){
     console.log(smallerNum)
 } */
+
 /* ciclo che moltiplica ogni volta x 2 il numero minore e lo va a sostituire al numero minore fuori dal ciclo */
+/* condizione: è il numero minore (dal primo cilo: moltiplicato x 2) ancora minore? ripeto il ciclo */
+
+for (i=0; i<100; i++ ){
+    if(inputPrint == false){
+        smallerNum *= 2
+    }else{
+        listaNumeriMaggiori.push(inputPrint)
+    }
+}
+
+console.log(listaNumeriMaggiori[0])
+console.log(listaNumeriMaggiori)
+
     
 /* condizione: è maggiore? mostro il numero in console*/
     
