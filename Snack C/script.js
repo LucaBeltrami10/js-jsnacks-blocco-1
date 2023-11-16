72bfb7d8bcb62efdf916858ca99054f08fda3bf5
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 18:01:18 +0100

    commentato condizione non funzionale all'esercizio

--- Snack C/script.js
+++ Snack C/script.js
@@ -1,9 +1,19 @@
 /* chiedo all'utente i due numeri */
-
+let smallerNum = prompt('inserisci un numero composto da 2 numeri')
+const largerNum = prompt('inserisci un numero composto da 4 numeri')
 
 /* mostro in console i inumeri inseriti */
 
+console.log(smallerNum)
+console.log(largerNum)
+
+
 /* condizione: è il numero minore (dal primo cilo: moltiplicato x 2) ancora minore? ripeto il ciclo */
+/* if(smallerNum <= largerNum){
+    smallerNum *= 2
+}else if(smallerNum > largerNum){
+    console.log(smallerNum)
+} */
 /* ciclo che moltiplica ogni volta x 2 il numero minore e lo va a sostituire al numero minore fuori dal ciclo */
     
 /* condizione: è maggiore? mostro il numero in console*/
