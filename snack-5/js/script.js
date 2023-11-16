68f4e5910de3e76ebfc6827690a90adafa6f52ba
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 12:28:10 +0100

    comparate le lunghezze parole

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -9,3 +9,11 @@ console.log(charactersFirstWord)
 
 let charactersSecondWord = secondWord.length
 console.log(charactersSecondWord)
+
+if(charactersFirstWord > charactersSecondWord){
+    console.log('la prima parola è la più lunga')
+}else if (charactersFirstWord < charactersSecondWord){
+    console.log('la seconda parola è la più lunga')
+}else (charactersFirstWord == charactersSecondWord){
+    console.log('le parole sono lunghe uguali')
+}
