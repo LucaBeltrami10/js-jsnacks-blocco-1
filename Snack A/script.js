fe75ea86b7ea62e5fdcf2ae5d62b08a468a72380
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 15:49:13 +0100

    richisto all'utente di inserire un numero

--- Snack A/script.js
+++ Snack A/script.js
@@ -1,5 +1,10 @@
 /* creo array vuoto per numeri dispari */
 const oddNumber = [];
 /* chiedo all'utente per 6 volte di inserire un numero da 1 a 100 */
-
+let userNum1 = prompt('inserisci un numero da 1 a 100')
+let userNum2 = prompt('inserisci un numero da 1 a 100')
+let userNum3 = prompt('inserisci un numero da 1 a 100')
+let userNum4 = prompt('inserisci un numero da 1 a 100')
+let userNum5 = prompt('inserisci un numero da 1 a 100')
+let userNum6 = prompt('inserisci un numero da 1 a 100')
 /* credo condizione: se è dispari lo inserisco nell'array di numeri dispari */
