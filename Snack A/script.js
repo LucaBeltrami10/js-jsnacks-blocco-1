07c599a00d56421256ea3d7569b4bee37c0275da
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 15:54:26 +0100

    inserito numero di richeieste da fare all'utente

--- Snack A/script.js
+++ Snack A/script.js
@@ -2,5 +2,7 @@
 const oddNumber = [];
 
 /* creo numero di richieste da fare all'utente */
+const numberOfRequest = 6
+
 /* creo ciclo in cui viene richiesto X volte all'utente di inserire un numero */
     /* comparo il numero inserito. se è dispari lo inserisco in lista sennò vado avanti*/
