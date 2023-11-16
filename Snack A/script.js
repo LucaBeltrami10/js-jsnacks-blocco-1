5eae716fd469fa25134304b9f914195547cb0d62
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 15:56:11 +0100

    creato ciclo

--- Snack A/script.js
+++ Snack A/script.js
@@ -5,4 +5,6 @@ const oddNumber = [];
 const numberOfRequest = 6
 
 /* creo ciclo in cui viene richiesto X volte all'utente di inserire un numero */
+for (let i=1; i<=6; i++){
+}
     /* comparo il numero inserito. se è dispari lo inserisco in lista sennò vado avanti*/
