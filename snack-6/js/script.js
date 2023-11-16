932f0508d0255320e6c96bdd95546e9eae69269f
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 12:45:07 +0100

    inserita condizione per entrare

--- snack-6/js/script.js
+++ snack-6/js/script.js
@@ -2,6 +2,10 @@ const listaInvitati = ['mario', 'gino', 'pino', 'maria','sara']
 
 let nomeUtente = prompt('inserisci il tuo nome')
 
-for (let=0; i<listaInvitati.length; i++){
-    
+for (let=0; i < listaInvitati.length ; i++){
+    if(nomeUtente == listaInvitati[i]){
+        console.log('PUOI ENTRARE!')
+    } else{
+        console.log('ON PUOI ENTRARE')
+    }
 }
