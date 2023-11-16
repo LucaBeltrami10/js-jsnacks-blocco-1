e6ee38808ef2f14dd453da1fd31b32059a3e3643
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 16:06:23 +0100

    print in console valori dispari inseriti. Finito

--- Snack A/script.js
+++ Snack A/script.js
@@ -1,14 +1,16 @@
 /* creo array vuoto per numeri dispari */
 const oddNumber = [];
 
-/* creo numero di richieste da fare all'utente */
+/* creo numero di richieste X da fare all'utente */
 const numberOfRequest = 6
 
 /* creo ciclo in cui viene richiesto X volte all'utente di inserire un numero */
 for (let i=1; i<=6; i++){
     let requestNumber = prompt('inserisci un numero da 1 a 100')
+    /* creo condizione in cui inserisco in array il numero se è dispari */
     if (requestNumber % 2 != 0){
         oddNumber.push(requestNumber)
     }
 }
-    /* comparo il numero inserito. se è dispari lo inserisco in lista sennò vado avanti*/
+/* visualizzo in console i numeri dispari inseriti in lista */
+console.log(oddNumber)
