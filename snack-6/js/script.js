b2204004dad689753b2b9caa5be982c59a0fc665
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 12:50:24 +0100

    corretto per mostrare il risultato puoi o non puoi

--- snack-6/js/script.js
+++ snack-6/js/script.js
@@ -1,11 +1,17 @@
 const listaInvitati = ['mario', 'gino', 'pino', 'maria','sara']
 
 let nomeUtente = prompt('inserisci il tuo nome')
-let esito;
+let esito = false
 
 for (let i=0; i< listaInvitati.length ; i++){
     if(nomeUtente == listaInvitati[i]){
-        esito = 'PUOI ENTRARE'
+        esito = true
         console.log(esito)
     }
+}
+
+if(esito == true){
+    console.log('puoi entrare')
+} else{
+    console.log('non puoi entrare')
 }
