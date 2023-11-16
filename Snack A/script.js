592643e23494ebad04f893fb551ef6303d8bb499
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 15:45:02 +0100

    commenti js per pianificare lavoro snack a

---
+++ Snack A/script.js
@@ -0,0 +1,5 @@
+/* creo array vuoto per numeri dispari */
+
+/* chiedo all'utente per 6 volte di inserire un numero da 1 a 100 */
+
+/* credo condizione: se è dispari lo inserisco nell'array di numeri dispari */
