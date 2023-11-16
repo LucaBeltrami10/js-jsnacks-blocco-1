9f93276d4f958aedac66ba00cba579864991e3ec
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 18:57:43 +0100

    corretto errore con variabile booleana. FINITO

--- Snack C/script.js
+++ Snack C/script.js
@@ -3,7 +3,6 @@ let smallerNum = parseInt(prompt('inserisci un numero composto da 2 numeri'))
 const largerNum = parseInt(prompt('inserisci un numero composto da 4 numeri'))
 
 /* mostro in console i inumeri inseriti */
-
 console.log(smallerNum)
 console.log(largerNum)
 
@@ -12,16 +11,15 @@ console.log(largerNum)
 creo un ciclo con (let i, i<numerodicilo; i++1)
 creo una condizione all'interno del cilo in cui aumento il numero di ciclo se una condizione non è soddisfatta
  */
-inputPrint = smallerNum > largerNum
-
 let numeroDiCicli = 1
 
 for (i=0; i<numeroDiCicli; i++){
+    inputPrint = smallerNum > largerNum
 
-    if (smallerNum < largerNum){
+    if (inputPrint == false){
         numeroDiCicli += 1
         smallerNum *= 2
-    } else if(largerNum < smallerNum){
+    } else{
         console.log(`numero di cili per arrivare al risultato: ${numeroDiCicli - 1}`)
         console.log(`moltiplicando il numero minore per 2, il primo numero che soddisfa la condizione numerominore > numero maggiore è: ${smallerNum}`)
         numeroDiCicli = 1
