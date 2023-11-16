e9f4008810b3ef268a3ffedbaa61767f4d73c3d4
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 16:03:24 +0100

    inserito valore se dispari in array

--- Snack A/script.js
+++ Snack A/script.js
@@ -7,5 +7,8 @@ const numberOfRequest = 6
 /* creo ciclo in cui viene richiesto X volte all'utente di inserire un numero */
 for (let i=1; i<=6; i++){
     let requestNumber = prompt('inserisci un numero da 1 a 100')
+    if (requestNumber % 2 != 0){
+        oddNumber.push(requestNumber)
+    }
 }
     /* comparo il numero inserito. se è dispari lo inserisco in lista sennò vado avanti*/
