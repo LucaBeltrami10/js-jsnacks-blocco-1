afd7749e88813cbea007b521f670390e367db415
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 18:48:43 +0100

    eliminati commenti con risoluzione sbagliata

--- Snack C/script.js
+++ Snack C/script.js
@@ -8,38 +8,10 @@ console.log(smallerNum)
 console.log(largerNum)
 
 
-
-/* const listaNumeriMaggiori = [] */
-
-/* if(smallerNum <= largerNum){
-    smallerNum *= 2
-}else if(smallerNum > largerNum){
-    console.log(smallerNum)
-} */
-
-/* ciclo che moltiplica ogni volta x 2 il numero minore e lo va a sostituire al numero minore fuori dal ciclo */
-/* condizione: è il numero minore (dal primo cilo: moltiplicato x 2) ancora minore? ripeto il ciclo */
-
-/* for (i=0; i<100; i++ ){
-    if(inputPrint == false){
-        smallerNum *= 2
-    }else{
-        listaNumeriMaggiori.push(inputPrint)
-    }
-}
-
-console.log(listaNumeriMaggiori[0])
-console.log(listaNumeriMaggiori) */
-
-    
-/* condizione: è maggiore? mostro il numero in console*/
-
 /* creo un numero di ciclo.
 creo un ciclo con (let i, i<numerodicilo; i++1)
 creo una condizione all'interno del cilo in cui aumento il numero di ciclo se una condizione non è soddisfatta
  */
-
-
 inputPrint = smallerNum > largerNum
 
 let numeroDiCicli = 1
@@ -56,4 +28,4 @@ for (i=0; i<numeroDiCicli; i++){
 }
     
 
-/* commento di risoluzione bug ciclo infinito. è necessario inserire nella condizione true (o larg < min) numero di cicli = 1 così da fermare il ciclo.*/
+/*NB commento di risoluzione bug ciclo infinito. è necessario inserire nella condizione true (o larg < min) numero di cicli = 1 così da fermare il ciclo.*/
