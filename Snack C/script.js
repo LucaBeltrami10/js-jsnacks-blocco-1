6ba4efe01706b4e361ede6b50a97f0bf45d28cc4
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 17:52:36 +0100

    commentato istruz. js per punti

---
+++ Snack C/script.js
@@ -0,0 +1,10 @@
+/* chiedo all'utente i due numeri */
+
+
+/* mostro in console i inumeri inseriti */
+
+/* condizione: è il numero minore (dal primo cilo: moltiplicato x 2) ancora minore? ripeto il ciclo */
+/* ciclo che moltiplica ogni volta x 2 il numero minore e lo va a sostituire al numero minore fuori dal ciclo */
+    
+/* condizione: è maggiore? mostro il numero in console */
+    
