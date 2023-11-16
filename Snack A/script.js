25b95886be357c716b4c55d392cdbfc0a5b1ac1e
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 15:46:08 +0100

    creato array vuoto

--- Snack A/script.js
+++ Snack A/script.js
@@ -1,5 +1,5 @@
 /* creo array vuoto per numeri dispari */
-
+const oddNumber = [];
 /* chiedo all'utente per 6 volte di inserire un numero da 1 a 100 */
 
 /* credo condizione: se è dispari lo inserisco nell'array di numeri dispari */
