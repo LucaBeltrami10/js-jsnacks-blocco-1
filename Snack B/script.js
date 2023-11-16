0912dae93aae52d374bc1a6afce6f348a9c4fa3c
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 16:57:47 +0100

    modificato contenuto ciclo

--- Snack B/script.js
+++ Snack B/script.js
@@ -2,22 +2,17 @@
 let userNumber = prompt('inserisci numero a 7 cifre');
 console.log(`il numero inserito è: ${userNumber}`);
 
-
-let prova = userNumber[0]
-console.log(prova)
-
-
 /* creo una variabile in cui sommo l'elaborazione del ciclo qui sotto */
 let sum;
-let numToAdd
+let numToAdd;
 
 /* creo ciclo i (7 cicli) in cui */
 for (let i=0; i<7; i++){
     /*variabile numDaSommare = userNumber[i]
     /* sommo  il singolo numero alla variabile somma*/
-    numToAdd = parseInt(userNumber[i])
-    console.log(`numero da sommare ${numToAdd}`)
-    sum = sum + numToAdd
+    numToAdd = userNumber[i]
+    console.log(`numero da sommare: ${numToAdd}`)
+    sum += numToAdd
 
 }
 
