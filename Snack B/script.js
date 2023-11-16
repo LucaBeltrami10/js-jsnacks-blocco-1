d3a720d1c78363954eb741ae4ebce877f30c3a0d
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 16:26:32 +0100

    commentnato per punti il lavoro in js

---
+++ Snack B/script.js
@@ -0,0 +1,10 @@
+/* richiedo all'utente un numero di 7 cifre */
+
+/* creo una variabile in cui sommo l'elaborazione del ciclo qui sotto */
+
+/* creo ciclo i (7 cicli) in cui */
+    /*variabile = numero richiesto dall'utente = text.split("",i) al valore inserito per selezionare il numero in posizione X del numero inserito dall'utente */
+    /* sommo  il singolo numero alla variabile somma*/
+
+/* mostro in console il risultato */
+
