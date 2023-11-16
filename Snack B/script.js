30666cee20021c797671d1bbb7bff42926c48eca
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-16 16:45:20 +0100

    modificati commenti guida. inserito ciclo

--- Snack B/script.js
+++ Snack B/script.js
@@ -1,9 +1,18 @@
 /* richiedo all'utente un numero di 7 cifre */
+let userNumber = prompt('inserisci numero a 7 cifre');
+console.log(`il numero inserito è: ${userNumber}`);
+
+
+let prova = userNumber[0]
+console.log(prova)
+
 
 /* creo una variabile in cui sommo l'elaborazione del ciclo qui sotto */
+let sum;
 
 /* creo ciclo i (7 cicli) in cui */
-    /*variabile = numero richiesto dall'utente = text.split("",i) al valore inserito per selezionare il numero in posizione X del numero inserito dall'utente */
+for (let i=0; i<7; i++)
+    /*variabile numDaSommare = userNumber[i]
     /* sommo  il singolo numero alla variabile somma*/
 
 /* mostro in console il risultato */
