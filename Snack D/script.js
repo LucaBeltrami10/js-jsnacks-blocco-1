25408fefd0d368f7e303e3c5ecbbf014fed2ab42
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-20 09:31:12 +0100

    snack D completato

--- Snack D/script.js
+++ Snack D/script.js
@@ -1,5 +1,18 @@
 const titolo = document.querySelector('h1')
 const bottone = document.querySelector('button')
 
+/* aggiungo alla variabile somma il numero randomico */
+const firstNumber = parseInt(Math.floor(Math.random()* 10) + 1) 
 
-titolo.innerHTML =  Math.floor(Math.random()* 10) + 1 
+let somma = firstNumber
+
+/* aggiungo il valore somma all'h1 in pagina */
+
+titolo.innerHTML = somma
+
+/* aggiungo listener su button che aggiunge la variabile somma aggiornata */
+
+bottone.addEventListener('click', function(){
+    somma = somma + 3
+    titolo.innerHTML = somma
+})
